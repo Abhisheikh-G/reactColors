823db5e412603a7157f53944dd706ff59d01c743
Abhisheikh Gill <abhisheikhg@gmail.com>
2019-08-15 15:04:40 -0400

    Add SnackBar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,23 +2,31 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 export default class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: "hex" };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { format: "hex", open: true };
+    this.handleFormatChange = this.handleFormatChange.bind(this);
+    this.closeSnackbar = this.closeSnackbar.bind(this);
   }
 
-  handleChange(e) {
-    this.setState({ format: e.target.value });
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
   }
+
+  closeSnackbar() {
+    this.setState({ open: false });
+  }
   render() {
-    const { level, changeLevel, handleChange } = this.props;
-    const { format } = this.state;
+    const { level, changeLevel } = this.props;
+    const { format, open } = this.state;
     return (
       <nav className="Navbar">
         <div className="logo">
@@ -37,12 +45,30 @@ export default class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select value={format} onChange={this.handleChange}>
-            <MenuItem value="hex">HEX - #fffff</MenuItem>
-            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
+          <Select value={format} onChange={this.handleFormatChange}>
+            <MenuItem value="hex">HEX - #ffffff </MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255) </MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0) </MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          autoHideDuration={3000}
+          onClose={this.closeSnackbar}
+          message={<span id="message-id">Format set to {format}</span>}
+          ContentProps={{ "aria-describedby": "message-id" }}
+          action={[
+            <IconButton>
+              <CloseIcon
+                onClick={this.closeSnackbar}
+                color="inherit"
+                key="close"
+                aria-label="close"
+              />
+            </IconButton>
+          ]}
+        />
       </nav>
     );
   }
