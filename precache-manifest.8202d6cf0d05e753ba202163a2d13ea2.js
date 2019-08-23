2c4f05aaaaf0adc67602c9bdaaf84e847a0aa282
Abhisheikh Gill <abhisheikhg@gmail.com>
2019-08-23 18:09:08 -0400

    Updates

---
+++ precache-manifest.8202d6cf0d05e753ba202163a2d13ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a3535ee5f260597d35a0d2a6f47234",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "85afb2730a6ca0a1172c",
+    "url": "/react-colors/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "2bf7ff15d54d939ff637",
+    "url": "/react-colors/static/css/main.cd3d43bd.chunk.css"
+  },
+  {
+    "revision": "85afb2730a6ca0a1172c",
+    "url": "/react-colors/static/js/2.d59cc7d8.chunk.js"
+  },
+  {
+    "revision": "2bf7ff15d54d939ff637",
+    "url": "/react-colors/static/js/main.69bb8bcb.chunk.js"
+  },
+  {
+    "revision": "d1fb0317b75dd9820162",
+    "url": "/react-colors/static/js/runtime~main.b00dc2c7.js"
+  },
+  {
+    "revision": "f3540bb22eab8df2a44b81cca46f890c",
+    "url": "/react-colors/static/media/bg.f3540bb2.svg"
+  }
+]);
